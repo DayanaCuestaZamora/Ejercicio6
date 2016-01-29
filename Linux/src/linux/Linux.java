0f576f8cd5a1c8ac1d38072e41e85fcf1325744e
DayanaCuestaZamora <cuesta.zamora@gmail.com>
2016-01-29 14:55:30 -0500

    Linux version 2.0

--- Linux/src/linux/Linux.java
+++ Linux/src/linux/Linux.java
@@ -15,7 +15,7 @@ public class Linux {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Linux version 1.0");
+        System.out.println("Linux version 2.0");
     }
     
 }
